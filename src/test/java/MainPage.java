11d3781239cde79785ebaf9caa39402c31bd1ec4
annatimofeyeva <anjatimofeyeva@gmail.com>
2020-10-11 11:38:52 -0700

    code refactoring

--- src/test/java/MainPage.java
+++ src/test/java/MainPage.java
@@ -16,6 +16,7 @@ public class MainPage {
     private String mainUrl = "https://romanceabroad.com/";
     private WebDriver driver;
     private WebDriverWait wait;
+    private String currentUrl;
     private String expectedUrlHome = "https://romanceabroad.com/#";
     private String expectedUrlHowWeWork = "https://romanceabroad.com/content/view/how-it-works";
     private String expectedUrlSearch = "https://romanceabroad.com/users/search";
@@ -53,9 +54,16 @@ public class MainPage {
     private By IFRAME = By.xpath("//iframe[contains(@src, 'https://www.youtube.com/embed/RRECuJzm3IY?start=85')]");
     private By BUTTON_FACEBOOK_REDIRACTION = By.xpath("//img[@src='/img/facebook-ico.jpg']");
     private By BUTTON_YouTube_REDIRACTION = By.xpath("//a[@href='https://www.youtube.com/channel/UCCmXAG1ToSyHymqYzzJcTfQ']");
-    private int indexValue = 0;
-
-
+    private By AGE_DROP_DOWN = By.cssSelector("#age_max");
+    private By BUTTON_SORTING_PHOTOS = By.cssSelector(".hidden-xs.fa.fa-arrow-down.pointer.plr5");
+    private By BUTTON_ADD_PHOTO = By.cssSelector(".caret");
+    private By LIST_OF_BESTSELLERS = By.xpath("//div[@class='g-users-gallery__item col-xs-4 col-md-3 col-lg-2 bestseller']");
+    private By TEXT_FIELD_GIFTS_SEARCH = By.cssSelector("#search_product");
+    private By BUTTON_GIFTS_SEARCH = By.cssSelector("#search_friend");
+    private By BUTTON_MAIN_NAV_BAR = By.cssSelector(".fa.fa-bars.fa-lg.item");
+    private By HEADER_TOURS_TO_UKRAIN = By.xpath("//div[@class='search-header']/h1");
+    private By BLOG_TITLE = By.xpath("//h1");
+    private int indexValueForYouTubeVideoElement = 0;
 
     @BeforeMethod
     public void setUp() {
@@ -66,55 +74,114 @@ public class MainPage {
         wait = (new WebDriverWait(driver, 20));
     }
 
-    //MainPage all Tabs Test cases - HomeWork4:
     @Test
     public void testHomeTab() {
         driver.findElement(LINK_HOME).click();
-        String currentUrl = driver.getCurrentUrl();
+        currentUrl = driver.getCurrentUrl();
         Assert.assertEquals(currentUrl, expectedUrlHome);
     }
 
     @Test
     public void testHowWeWorkTab() {
         driver.findElement(LINK_HOW_WORK).click();
-        String currentUrlHowWeWork = driver.getCurrentUrl();
-        Assert.assertEquals(currentUrlHowWeWork, expectedUrlHowWeWork);
+        currentUrl = driver.getCurrentUrl();
+        Assert.assertEquals(currentUrl, expectedUrlHowWeWork);
     }
 
     @Test
     public void testPrettyWomanTab() {
         driver.findElement(LINK_SEARCH).click();
-        String currentUrlSearch = driver.getCurrentUrl();
-        Assert.assertEquals(currentUrlSearch, expectedUrlSearch);
+        currentUrl = driver.getCurrentUrl();
+        Assert.assertEquals(currentUrl, expectedUrlSearch);
+    }
+
+    @Test
+    public void testPrettyWomenMaxAgeChaneDropDown() {
+        driver.findElement(LINK_SEARCH).click();
+        WebElement dropDown =
+                wait.until(ExpectedConditions.presenceOfElementLocated(AGE_DROP_DOWN));
+        dropDown.click();
     }
 
     @Test
     public void testPhotosTab() {
         driver.findElement(LINK_PHOTOS).click();
-        String currentUrlPhotos = driver.getCurrentUrl();
-        Assert.assertEquals(currentUrlPhotos, expectedUrlPhotos);
+        currentUrl = driver.getCurrentUrl();
+        Assert.assertEquals(currentUrl, expectedUrlPhotos);
+    }
+
+    @Test
+    public void testPhotosSorting() {
+        driver.findElement(LINK_PHOTOS).click();
+        WebElement sortingPhotoButton =
+                wait.until(ExpectedConditions.presenceOfElementLocated(BUTTON_SORTING_PHOTOS));
+        sortingPhotoButton.click();
+    }
+
+    @Test
+    public void testAddPhotoButton() {
+        driver.findElement(LINK_PHOTOS).click();
+        WebElement addPhotoButton = wait.until(ExpectedConditions.presenceOfElementLocated(BUTTON_ADD_PHOTO));
+        addPhotoButton.click();
     }
 
     @Test
     public void testGiftsTab() {
         driver.findElement(LINK_GIFTS).click();
-        String currentUrlGifts = driver.getCurrentUrl();
-        Assert.assertEquals(currentUrlGifts, expectedUrlGifts);
+        currentUrl = driver.getCurrentUrl();
+        Assert.assertEquals(currentUrl, expectedUrlGifts);
     }
 
     @Test
-    public void testToutToUkraineTab() throws InterruptedException {
+    public void testGiftsTabBestsellersSelected() {
+        driver.findElement(LINK_GIFTS).click();
+        currentUrl = driver.getCurrentUrl();
+        List bestsellers =
+                wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(LIST_OF_BESTSELLERS));
+        //System.out.println(bestsellers.size());
+        Assert.assertEquals(currentUrl, expectedUrlGifts);
+        Assert.assertTrue(bestsellers.size() > 0);
+    }
+
+    @Test
+    public void testSearchForExactGift() {
+        driver.findElement(LINK_GIFTS).click();
+        WebElement giftSearchField =
+                wait.until(ExpectedConditions.presenceOfElementLocated(TEXT_FIELD_GIFTS_SEARCH));
+        giftSearchField.sendKeys("Flower busket");
+        WebElement giftSearchButton = driver.findElement(BUTTON_GIFTS_SEARCH);
+        giftSearchButton.click();
+    }
+
+    @Test
+    public void testSearchForGiftsAndClickMainNavBar() {
+        driver.findElement(LINK_GIFTS).click();
+        WebElement mainNavBarButton =
+                wait.until(ExpectedConditions.presenceOfElementLocated(BUTTON_MAIN_NAV_BAR));
+        mainNavBarButton.click();
+    }
+
+    @Test
+    public void testToutToUkraineTab() {
         driver.findElement(LINK_TOUR).click();
-        String currentUrlTours = driver.getCurrentUrl();
-        Thread.sleep(3000);
-        Assert.assertEquals(currentUrlTours, expectedUrlTours);
+        currentUrl = driver.getCurrentUrl();
+        String searchToursToUkraineHeader =
+                wait.until(ExpectedConditions.presenceOfElementLocated(HEADER_TOURS_TO_UKRAIN))
+                        .getText();
+        Assert.assertEquals(expectedToursToUkraineTitle, searchToursToUkraineHeader);
+        Assert.assertEquals(currentUrl, expectedUrlTours);
+
     }
 
     @Test
     public void testBlogTab() {
         driver.findElement(LINK_BLOG).click();
-        String currentUrlBlog = driver.getCurrentUrl();
-        Assert.assertEquals(currentUrlBlog, expectedUrlBlog);
+        currentUrl = driver.getCurrentUrl();
+        String blogTitle =
+                wait.until(ExpectedConditions.presenceOfElementLocated(BLOG_TITLE))
+                        .getText();
+        Assert.assertEquals(currentUrl, expectedUrlBlog);
+        Assert.assertEquals(expectedBlogPageTitle, blogTitle);
     }
 
     @Test
@@ -127,16 +194,14 @@ public class MainPage {
         driver.findElement(BUTTON_SIGN_IN).click();
         WebElement error_popUp = wait.until(ExpectedConditions.presenceOfElementLocated(ERROR_POP_UP));
         String currentBackgroundColor = error_popUp.getCssValue("border-color");
-        System.out.println(currentBackgroundColor);
+        //System.out.println(currentBackgroundColor);
         Assert.assertEquals(currentBackgroundColor, expectedBackgroundColor);
-
         String errorPopUpText = error_popUp.getText();
-        System.out.println(errorPopUpText);
+        //System.out.println(errorPopUpText);
         Assert.assertEquals(errorPopUpText, expectedLoginErrorPopUpText);
-
         List<WebElement> errorPopUp =
                 wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(ERROR_POP_UP));
-        System.out.println(errorPopUp.size());
+        //System.out.println(errorPopUp.size());
         Assert.assertEquals(errorPopUp.size(), 1);
         Assert.assertTrue(errorPopUp.size() > 0);
     }
@@ -144,20 +209,20 @@ public class MainPage {
     @Test
     public void testBookNowTab() {
         driver.findElement(LINK_BOOK_NOW).click();
-        String currentUrlBookNow = driver.getCurrentUrl();
-        Assert.assertEquals(currentUrlBookNow, expectedUrlTours);
+        currentUrl = driver.getCurrentUrl();
+        Assert.assertEquals(currentUrl, expectedUrlTours);
     }
 
     @Test
     public void testJoinForFreeButton() {
         driver.findElement(BUTTON_JOIN_FOR_FREE).click();
-        String emailFieldTitle = wait.until(ExpectedConditions.presenceOfElementLocated(TEXT_FIELD_EMAIL))
-                .getText();
-        //System.out.println(emailFieldTitle);
+        String emailFieldTitle =
+                wait.until(ExpectedConditions.presenceOfElementLocated(TEXT_FIELD_EMAIL))
+                        .getText();
         Assert.assertEquals(expectedEmailFieldTitle, emailFieldTitle);
     }
 
-    // ?? Verifying successfully file downloading ??
+    // ? How to assert file downloading
     // https://www.seleniumeasy.com/selenium-tutorials/verify-file-after-downloading-using-webdriver-java
     @Test
     public void testRequestTourButton() {
@@ -167,7 +232,7 @@ public class MainPage {
         requestTourButton.click();
     }
 
-    // How to make an Assertion that video is playing, how to stop/ replay video in IFrame ??
+    // How to assert video playing, how to stop/ replay video in IFrame??
     @Test
     public void testPlayVideoButton() {
         JavascriptExecutor jse = (JavascriptExecutor) driver;
@@ -190,207 +255,8 @@ public class MainPage {
     public void testYouTubeRedirectionButton() {
         JavascriptExecutor js = ((JavascriptExecutor) driver);
         js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
-        driver.findElements(BUTTON_FACEBOOK_REDIRACTION).get(0).click();
+        driver.findElements(BUTTON_FACEBOOK_REDIRACTION).get(indexValueForYouTubeVideoElement).click();
     }
-
-    @Test
-    public void testCarousel() {
-        JavascriptExecutor js = ((JavascriptExecutor) driver);
-        js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
-        driver.findElements(BUTTON_FACEBOOK_REDIRACTION).get(indexValue).click();
-    }
-
-
-//    // Xpath locators:
-//    @Test
-//    public void testPrettyWomenSearchTab() {
-//        WebElement prettyWomenTab = driver.findElement(By.xpath("//a[text()='PRETTY WOMEN']"));
-//        prettyWomenTab.click();
-//        String currentUrlSearch = driver.getCurrentUrl();
-//        Assert.assertEquals(currentUrlSearch, expectedUrlSearch);
-//    }
-//
-//    @Test
-//    public void testPrettyWomenSearchTabWithCssSelector() {
-//        WebElement prettyWomenTab = driver.findElement(By.cssSelector("a[href='https://romanceabroad.com/users/search'"));
-//        prettyWomenTab.click();
-//        String expectedUrl = driver.getCurrentUrl();
-//        Assert.assertEquals(expectedUrl, expectedUrlSearch);
-//    }
-//
-//    @Test
-//    public void testPhotosTab2() {
-//        WebElement photosTab = driver.findElement(By.xpath("//a[text()='PHOTOS']"));
-//        photosTab.click();
-//        String currentUrlPhotos = driver.getCurrentUrl();
-//        Assert.assertEquals(currentUrlPhotos, expectedUrlPhotos);
-//    }
-//
-//    @Test
-//    public void testGiftsTab2() {
-//        WebElement giftsTab = driver.findElement(By.xpath("//a[@href='https://romanceabroad.com/store/category-sweets']"));
-//        giftsTab.click();
-//        String currentUrlGifts = driver.getCurrentUrl();
-//        List bestsellers = wait
-//                .until(ExpectedConditions.presenceOfAllElementsLocatedBy(By
-//                        .xpath("//div[@class='g-users-gallery__item col-xs-4 col-md-3 col-lg-2 bestseller']")));
-//        //System.out.println(bestsellers.size());
-//        Assert.assertEquals(currentUrlGifts, expectedUrlGifts);
-//        //Assert.assertTrue(bestsellers.size() > 0);
-//    }
-//
-//    @Test
-//    public void testTourToUkraineTab() {
-//        WebElement tourToUkraineTab = driver.findElement(By.xpath("//a[contains(text(), 'TOUR TO')]"));
-//        tourToUkraineTab.click();
-//        String searchToursToUkraineHeader = wait
-//                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='search-header']/h1")))
-//                .getText();
-//        //System.out.println(searchToursToUkraineHeader);
-//        Assert.assertEquals(expectedToursToUkraineTitle, searchToursToUkraineHeader);
-//    }
-//
-//    @Test
-//    public void testBlogTab2() {
-//        WebElement blogTab = driver.findElement(By.xpath("//a[text()='BLOG']"));
-//        blogTab.click();
-//        String currentUrlBlog = driver.getCurrentUrl();
-//        String blogTitle = wait
-//                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//h1")))
-//                .getText();
-//        //System.out.println(blogTitle);
-//        Assert.assertEquals(currentUrlBlog, expectedUrlBlog);
-//        Assert.assertEquals(expectedBlogPageTitle, blogTitle);
-//    }
-//
-//    @Test
-//    public void testBookNowTab2() {
-//        WebElement bookNowTab = driver.findElement(By.xpath("//a[@href='https://romanceabroad.com/store/sweets/20-tour_to_ukraine']"));
-//        bookNowTab.click();
-//        String discountNumbers = wait.until(ExpectedConditions.presenceOfElementLocated(By
-//                .cssSelector(".mt20"))).getText();
-//        //System.out.println(discountNumbers);
-//        String tourDiscountPrice = driver.findElement(By.cssSelector(".bold.view-price.fs18")).getText();
-//        //System.out.println(tourDiscountPrice);
-//        Assert.assertEquals(expectedDiscount, tourDiscountPrice);
-//    }
-//
-//    @Test
-//    public void testHomeTabCss() {
-//        WebElement homeTab = driver.findElement(By.xpath("//a[@href='#'][text()='HOME']"));
-//        homeTab.click();
-//        String homeTabUrl = driver.getCurrentUrl();
-//        Assert.assertEquals(expectedUrlHome, homeTabUrl);
-//    }
-//
-//    @Test
-//    public void testJoinTodayButton() throws InterruptedException {
-//        WebElement requestTourButton = driver.findElement(By.xpath("//a[@href='#'][text()='JOIN TODAY!']"));
-//        JavascriptExecutor js = ((JavascriptExecutor) driver);
-//        js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
-//        Thread.sleep(5000);
-//        requestTourButton.click();
-//        String emailFieldTitle = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//label[@for='email']")))
-//                .getText();
-//        //System.out.println(emailFieldTitle);
-//        Assert.assertEquals(expectedEmailFieldTitle, emailFieldTitle);
-//    }
-//
-//    @Test
-//    public void testFacebookIntegrationButton() {
-//        WebElement facebookIntegrationButton = driver.findElement(By.xpath("//img[@src='/img/facebook-ico.jpg']"));
-//        JavascriptExecutor js = ((JavascriptExecutor) driver);
-//        js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
-//        facebookIntegrationButton.click();
-//    }
-//
-//    // CssSelector locators:
-//    @Test
-//    public void testSignInTab2() {
-//        WebElement signInTab = driver.findElement(By.cssSelector("#ajax_login_link"));
-//        signInTab.click();
-//        WebElement loginFormTitleDynamicElement = wait
-//                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='content-block load_content']/h1")));
-//        String loginFormTitle = loginFormTitleDynamicElement.getText();
-//        //System.out.println(loginFormTitle);
-//        Assert.assertEquals(expectedLoginFormTitle, loginFormTitle);
-//    }
-//
-//    @Test
-//    public void testJoinForFreeButton2() {
-//        WebElement joinForFreeButton = driver.findElement(By.cssSelector("#show-registration-block"));
-//        joinForFreeButton.click();
-//        String emailFieldTitle = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//label[@for='email']")))
-//                .getText();
-//        //System.out.println(emailFieldTitle);
-//        Assert.assertEquals(expectedEmailFieldTitle, emailFieldTitle);
-//    }
-//
-//    @Test
-//    public void testSearchForGiftsTextField() {
-//        driver.findElement(By.xpath("//a[@href='https://romanceabroad.com/store/category-sweets']")).click();
-//        WebElement giftSearchField = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#search_product")));
-//        giftSearchField.sendKeys("Flower busket");
-//    }
-//
-//    @Test
-//    public void testSearchForExactGift() {
-//        WebElement giftsTab = driver.findElement(By.xpath("//a[@href='https://romanceabroad.com/store/category-sweets']"));
-//        giftsTab.click();
-//        WebElement giftSearchField = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#search_product")));
-//        giftSearchField.sendKeys("Flower busket");
-//        WebElement giftSearchButton = driver.findElement(By.cssSelector("#search_friend"));
-//        giftSearchButton.click();
-//    }
-//
-//    @Test
-//    public void testSearchForGiftsAndClickOnNavBar() {
-//        WebElement giftsTab = driver.findElement(By.xpath("//a[@href='https://romanceabroad.com/store/category-sweets']"));
-//        giftsTab.click();
-//        WebElement navBarButton = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".fa.fa-bars.fa-lg.item")));
-//        navBarButton.click();
-//    }
-//
-//    @Test
-//    public void testPrettyWomenMinAgeChaneDropDown() {
-//        WebElement prettyWomenTab = driver.findElement(By.xpath("//a[text()='PRETTY WOMEN']"));
-//        prettyWomenTab.click();
-//        WebElement dropDown = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#age_min")));
-//        dropDown.click();
-//    }
-//
-//    @Test
-//    public void testPrettyWomenMaxAgeChaneDropDown() {
-//        WebElement prettyWomenTab = driver.findElement(By.xpath("//a[text()='PRETTY WOMEN']"));
-//        prettyWomenTab.click();
-//        WebElement dropDown = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#age_max")));
-//        dropDown.click();
-//    }
-//
-//    @Test
-//    public void testPrettyWomenTabMainSearchButton() {
-//        WebElement prettyWomenTab = driver.findElement(By.xpath("//a[text()='PRETTY WOMEN']"));
-//        prettyWomenTab.click();
-//        WebElement mainSearchButton = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#main_search_button_user_advanced")));
-//        mainSearchButton.click();
-//    }
-//
-//    @Test
-//    public void testAddPhotoButton() {
-//        WebElement photosTab = driver.findElement(By.xpath("//a[@href='https://romanceabroad.com/media/index']"));
-//        photosTab.click();
-//        WebElement addPhotoButton = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".caret")));
-//        addPhotoButton.click();
-//    }
-//
-//    @Test
-//    public void testPhotosSorting() {
-//        WebElement photosTab = driver.findElement(By.xpath("//a[@href='https://romanceabroad.com/media/index']"));
-//        photosTab.click();
-//        WebElement sortingPhotoButton = wait
-//                .until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".hidden-xs.fa.fa-arrow-down.pointer.plr5")));
-//        sortingPhotoButton.click();
-//    }
 
     @AfterMethod
     public void tearDown() {
