7a7db61dbc8fe3889010a858d0b98243bba7b7eb
annatimofeyeva <anjatimofeyeva@gmail.com>
2020-11-22 16:41:48 -0800

    fix StaleElementReferenceException

--- src/test/java/MainPageTests.java
+++ src/test/java/MainPageTests.java
@@ -1,8 +1,6 @@
-import org.omg.PortableInterceptor.LOCATION_FORWARD;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -102,46 +100,144 @@ public class MainPageTests extends BaseUI {
     public void testClickNavigationTabs() {
 
         List<WebElement> list =
-                wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//ul[@class='navbar-nav']//li")));
+                wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//div[@class='collapse navbar-collapse']//ul//li")));
         for (int i = 0; i < list.size(); i++) {
            /* resolving  StaleElementReferenceException;
             OpenQA.Selenium.StaleElementReferenceException: stale element reference: element is not attached to the page document*/
-            list = wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//ul[@class='navbar-nav']//li")));
-            String info = list.get(i).getText();
+            list = wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//div[@class='collapse navbar-collapse']//ul//li")));
             list.get(i).click();
-
-            if (info.equals("HOME")) {
-                System.out.println(driver.getCurrentUrl());
-                Assert.assertEquals(Data.expectedUrlHome, driver.getCurrentUrl());
-
-            }
-            // +
-            if (info.contains("WORK")) {
-                System.out.println(driver.getCurrentUrl());
-                wait.until(ExpectedConditions.elementToBeClickable(Locators.PAGE_TITLE));
-                String actualTitle = driver.findElement(Locators.PAGE_TITLE).getText();
-                Assert.assertEquals(Data.expectedTitleHowItWoks, actualTitle);
-            }
-
-            if (info.equals("PRETTY WOMEN")) {
-                System.out.println(driver.getCurrentUrl());
-                WebElement firstImage =
-                        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@title='Tanua']")));
-                //firstImage.click();
-                System.out.println(firstImage);
-                boolean firstPhoto = firstImage.isDisplayed();
-                if (firstPhoto) {
-                    Assert.assertEquals(Data.expectedUrlSearch, driver.getCurrentUrl());
-                } else {
-                    Assert.fail("Photos are not displayed");
-            }
-
-
             driver.get(Data.mainUrl);
-            list = driver.findElements(By.xpath("//ul[@class='navbar-nav']//li"));
+            list = wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//div[@class='collapse navbar-collapse']//ul//li")));
 
         }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        List<WebElement> list =
+//                wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//ul[@class='navbar-nav']//li")));
+//        for (int i = 0; i < list.size(); i++) {
+//           /* resolving  StaleElementReferenceException;
+//            OpenQA.Selenium.StaleElementReferenceException: stale element reference: element is not attached to the page document*/
+//            list = wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//ul[@class='navbar-nav']//li")));
+//            String info = list.get(i).getText();
+//            list.get(i).click();
+//
+//            if (info.equals("HOME")) {
+//                System.out.println(driver.getCurrentUrl());
+//                Assert.assertEquals(Data.expectedUrlHome, driver.getCurrentUrl());
+//
+//            }
+//            // +
+//            if (info.contains("WORK")) {
+//                System.out.println(driver.getCurrentUrl());
+//                wait.until(ExpectedConditions.elementToBeClickable(Locators.PAGE_TITLE));
+//                String actualTitle = driver.findElement(Locators.PAGE_TITLE).getText();
+//                Assert.assertEquals(Data.expectedTitleHowItWoks, actualTitle);
+//            }
+//
+//            if (info.equals("PRETTY WOMEN")) {
+//                System.out.println(driver.getCurrentUrl());
+//                WebElement firstImage =
+//                        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@title='Tanua']")));
+//                //firstImage.click();
+//                System.out.println(firstImage);
+//                boolean firstPhoto = firstImage.isDisplayed();
+//                if (firstPhoto) {
+//                    Assert.assertEquals(Data.expectedUrlSearch, driver.getCurrentUrl());
+//                } else {
+//                    Assert.fail("Photos are not displayed");
+//                }
+//
+//                try {
+//                    Thread.sleep(3000);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//                driver.navigate().back();
+//                list = driver.findElements(By.xpath("//ul[@class='navbar-nav']//li"));
+//
+//            }
+//        }
     }
 
-} // end of class
+
+
+
+
+//        List<WebElement> list =
+//                wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//ul[@class='navbar-nav']//li")));
+//        for (int i = 0; i < list.size(); i++) {
+//           /* resolving  StaleElementReferenceException;
+//            OpenQA.Selenium.StaleElementReferenceException: stale element reference: element is not attached to the page document*/
+//            list = wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//div[@id='navbarResponsive']//ul//li")));
+//            //mainPage.ajaxClick(list.get(i));
+//           list.get(i).click();
+//           String info =  list.get(i).getText();
+//
+//            if (info.equals("HOME")) {
+//                System.out.println(driver.getCurrentUrl());
+//                Assert.assertEquals(Data.expectedUrlHome, driver.getCurrentUrl());
+//
+//            }
+//
+//            if (info.contains("WORK")) {
+//                Assert.assertEquals(Data.expectedUrlHowWeWork,driver.getCurrentUrl());
+//            }
+//
+//            if (info.equals("PRETTY WOMEN")) {
+//                System.out.println(driver.getCurrentUrl());
+//                WebElement firstImage =
+//                        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@title='Tanua']")));
+//                //firstImage.click();
+//                System.out.println(firstImage);
+//                boolean firstPhoto = firstImage.isDisplayed();
+//                if (firstPhoto) {
+//                    Assert.assertEquals(Data.expectedUrlSearch, driver.getCurrentUrl());
+//                } else {
+//                    Assert.fail("Photos are not displayed");
+//                }
+//
+//
+//                if (info.equals("PHOTOS")) {
+//                    System.out.println(driver.getCurrentUrl());
+//                    List<WebElement> links =
+//                            wait.until(ExpectedConditions.visibilityOfAllElements((List<WebElement>) By.xpath("//ul[@class='b-tabs']")));
+//                    int linksSize = links.size();
+//                    if (linksSize == 4) {
+//                        System.out.println("All links are displayed");
+//                    } else {
+//                        Assert.fail("Not all links displayed in top page navBar");
+//                    }
+//                    System.out.println(driver.getCurrentUrl());
+//                    Assert.assertEquals(Data.expectedUrlPhotos, driver.getCurrentUrl());
+//                    try {
+//                        Thread.sleep(2000);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+////                    }
+//                driver.navigate().back();
+//                list = wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//ul[@class='navbar-nav']//li")));
+//
+//            }
+//
+//        }
+   //}
+}
+
+
+
