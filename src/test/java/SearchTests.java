14e829fbd1b6c9dc7b7322e817ffa9ef847dd082
annatimofeyeva <anjatimofeyeva@gmail.com>
2020-12-03 22:32:43 -0800

    add priority to annotation @Test in SearchTests

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -3,33 +3,33 @@ import org.testng.annotations.Test;
 
 public class SearchTests extends BaseUI {
 
-    @Test
+    @Test(priority = 5)
     public void testSearchWomenByName() {
         String actualTextSelectedINDropDown = searchPage.searchWomenByName();
         Assert.assertEquals(Data.expectedTextSelectedInDropDown, actualTextSelectedINDropDown);
     }
 
-    @Test
+    @Test(priority = 4)
     public void testSearchWomenByAges() {
         String actualNumberOfWomen = searchPage.searchWomenByAges();
         Assert.assertEquals(Data.expectedSearchResultSize, actualNumberOfWomen);
     }
 
-    @Test
+    @Test(priority = 3)
     public void testRandomSearchFromDropDown() {
         searchPage.randomSearchFromDropDown();
     }
 
-    @Test
+    @Test(priority = 2)
     public void testRandomSearchFromDropDownSecond() {
         searchPage.randomSearchFromDropDownSecond();
     }
 
-    @Test
+    @Test(priority = 1)
     public void testGetSizeDropDownList() {
         int actualSize = searchPage.getSizeDropDownList();
         System.out.println("Size of DropDown list: " + actualSize);
-        Assert.assertEquals(actualSize, Data.expectedSizeOfDropDown);
+        Assert.assertEquals(actualSize, Data.expectedSizeOfDropDownList);
     }
 }
 
