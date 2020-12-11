4ae348c7944c60e81211b39869b333718d70fe8d
annatimofeyeva <anjatimofeyeva@gmail.com>
2020-12-10 16:04:35 -0800

    add first steps for DataProvider setting

--- src/test/java/RegistrationTests.java
+++ src/test/java/RegistrationTests.java
@@ -1,21 +1,29 @@
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class RegistrationTests extends BaseUI {
 
-    @Test
-    public void testRegistration() {
+    @DataProvider(name="Registration")
+    public static Object[][] testRegisration() throws Exception {
+        ArrayList<Object[]> out = new ArrayList<>();
+        Files.readAllLines(Paths.get("SearchGifts.cvs")).stream().forEach(s-> {
+            String[] data = s.split(",");
+            out.add(new Object[]{data[0]});
+        });
+        return out.toArray(new Object[out.size()][]);
+    }
+
+    @Test(dataProvider = "Registration")
+    public void testRegistration(String email) {
+        System.out.println(email);
         mainPage.clickJoinButton();
         mainPage.completeFirstPartOfRegistration(Data.email, Data.password);
         mainPage.completeSecondPartOfRegistration(mainPage.generateUniqueUserName(Data.userName, 5), Data.month, Data.day,
                 Data.year, Data.phone, Data.city, Data.location);
-
-//        Assert.assertTrue(!driver.findElement(Locators.LINK_SEARCH).isDisplayed(), "Element is not displayed");
-//        checkboxConformation.click();
-//        if (!driver.findElement(Locators.LINK_SEARCH).isSelected()) {
-//            checkboxConformation.click();
-//        } else {
-//            Assert.fail("Checkbox is already selected");
-//        }
     }
 }
 
