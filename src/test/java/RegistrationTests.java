ed9709ecbe9dbdb246c0a4f6571527580e811b21
annatimofeyeva <anjatimofeyeva@gmail.com>
2020-12-10 17:33:42 -0800

    add next steps for Registration test wir DataProvider

--- src/test/java/RegistrationTests.java
+++ src/test/java/RegistrationTests.java
@@ -10,20 +10,18 @@ public class RegistrationTests extends BaseUI {
     @DataProvider(name="Registration")
     public static Object[][] testRegisration() throws Exception {
         ArrayList<Object[]> out = new ArrayList<>();
-        Files.readAllLines(Paths.get("SearchGifts.cvs")).stream().forEach(s-> {
+        Files.readAllLines(Paths.get("Registration.csv")).stream().forEach(s-> {
             String[] data = s.split(",");
-            out.add(new Object[]{data[0]});
+            out.add(new Object[]{data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7]});
         });
         return out.toArray(new Object[out.size()][]);
     }
 
     @Test(dataProvider = "Registration")
-    public void testRegistration(String email) {
-        System.out.println(email);
+    public void testRegistration(String email, String password, String day, String month, String year, String phone, String city, String location) {
         mainPage.clickJoinButton();
-        mainPage.completeFirstPartOfRegistration(Data.email, Data.password);
-        mainPage.completeSecondPartOfRegistration(mainPage.generateUniqueUserName(Data.userName, 5), Data.month, Data.day,
-                Data.year, Data.phone, Data.city, Data.location);
+        mainPage.completeFirstPartOfRegistration(email, password);
+        mainPage.completeSecondPartOfRegistration(mainPage.generateUniqueUserName(Data.userName, 5), phone, month, day, year, city, location);
     }
 }
 
