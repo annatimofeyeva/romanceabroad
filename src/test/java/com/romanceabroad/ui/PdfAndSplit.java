2c0c1ef89c9be2c9798cc190e914ecefa5c55ec1
annatimofeyeva <anjatimofeyeva@gmail.com>
2020-12-20 15:00:41 -0800

    add testSplit()

--- src/test/java/com/romanceabroad/ui/PdfAndSplit.java
+++ src/test/java/com/romanceabroad/ui/PdfAndSplit.java
@@ -7,28 +7,35 @@ import org.testng.annotations.Test;
 import java.io.IOException;
 
 public class PdfAndSplit {
-    //test is not passed, because i have no pdf files to compare in the project
-  @Test
+    //test is not passed, because i have no pdf files to compare in the project - just nice to have library
+    @Test
     public void testPdfComparison() throws IOException {
-      new PdfComparator("expected.pdf", "actual.pdf").compare().writeTo("diffOutput");
+        new PdfComparator("expected.pdf", "actual.pdf").compare().writeTo("diffOutput");
 
-      //The compare-method returns a CompareResult, which can be queried:
-      final CompareResult result = new PdfComparator("expected.pdf", "actual.pdf").compare();
-      if (result.isNotEqual()) {
-          System.out.println("Differences found!");
-      }
-      if (result.isEqual()) {
-          System.out.println("No Differences found!");
-      }
-      if (result.hasDifferenceInExclusion()) {
-          System.out.println("Differences in excluded areas found!");
-      }
-      result.getDifferences(); // returns page areas, where differences were found
+        //The compare-method returns a CompareResult, which can be queried:
+        final CompareResult result = new PdfComparator("expected.pdf", "actual.pdf").compare();
+        if (result.isNotEqual()) {
+            System.out.println("Differences found!");
+        }
+        if (result.isEqual()) {
+            System.out.println("No Differences found!");
+        }
+        if (result.hasDifferenceInExclusion()) {
+            System.out.println("Differences in excluded areas found!");
+        }
+        result.getDifferences(); // returns page areas, where differences were found
 
-      //For convenience, writeTo also returns the equals status:
-      boolean isEquals = new PdfComparator("expected.pdf", "actual.pdf").compare().writeTo("diffOutput");
-      if (!isEquals) {
-          System.out.println("Differences found!");
-      }
-  }
+        //For convenience, writeTo also returns the equals status:
+        boolean isEquals = new PdfComparator("expected.pdf", "actual.pdf").compare().writeTo("diffOutput");
+        if (!isEquals) {
+            System.out.println("Differences found!");
+        }
+    }
+    @Test
+    public void testSplit() {
+        String info = "Name, 23";
+        String[] phraseSplited= info.split(", ");
+        String age = phraseSplited[1];
+        System.out.println(age);
+    }
 }
