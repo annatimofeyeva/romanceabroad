78dd7de09c4a9d8f4baa21ec9696dd6b2668260a
annatimofeyeva <anjatimofeyeva@gmail.com>
2020-12-22 21:36:39 -0800

    add steps to testUserTabs()

--- src/test/java/com/romanceabroad/ui/MediaTests.java
+++ src/test/java/com/romanceabroad/ui/MediaTests.java
@@ -1,6 +1,8 @@
 package com.romanceabroad.ui;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -24,14 +26,34 @@ public class MediaTests extends BaseUI {
         for (int i = 0; i < userTabs.size(); i++) {
             userTabs.get(i).click();
             actualTitle = mediaPage.getAnyTitle(Locators.H1_TITLE);
+/*
+            if (actualTitle.contains(Data.expectedTitleGallery) || actualTitle.contains(Data.expectedTitlePhotoGallery)
+                    || actualTitle.contains(Data.expectedTitleVideoGallery) || actualTitle.contains(Data.expectedTitleAlbumsGallery)) {
+                System.out.println("Title is valid: " + actualTitle);
+            }
+            } else {
+                Assert.fail("Title is not valid");
+            }
+*/
             if (i == 0) {
                 Assert.assertEquals(actualTitle, Data.expectedTitleGallery);
             } else if (i == 1) {
                 Assert.assertEquals(actualTitle, Data.expectedTitlePhotoGallery);
             } else if (i == 2) {
                 Assert.assertEquals(actualTitle, Data.expectedTitleVideoGallery);
+                WebElement textElement = wait.until(ExpectedConditions.elementToBeClickable
+                                (By.xpath("//div[@class='main-inner-content']//div[@class='wrapper']//div[text()='No media']")));
+                String textMedia = textElement.getText();
+                System.out.println(textMedia);
             } else if (i == 3) {
                 Assert.assertEquals(actualTitle, Data.expectedTitleAlbumsGallery);
+                WebElement photoElement =  wait.until(ExpectedConditions.elementToBeClickable
+                        (By.xpath("//div[@id='gallery']//div[@title='Portrait']")));
+                boolean displayedPhotoElement = photoElement.isDisplayed();
+                String actualPhotoElementTitle = photoElement.getText();
+                System.out.println(actualPhotoElementTitle);
+                Assert.assertTrue(displayedPhotoElement);
+                Assert.assertEquals(actualPhotoElementTitle, "Portrait");
             }
             userTabs = driver.findElements(Locators.LINK_PHOTOS_ALL);
         }
