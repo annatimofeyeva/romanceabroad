083e5e250b3ff5328661e20eb7bcb5a2be3e3416
annatimofeyeva <anjatimofeyeva@gmail.com>
2021-02-01 15:55:58 -0800

    add mobile optimization code to BaseUI

--- src/test/java/com/romanceabroad/ui/BaseUI.java
+++ src/test/java/com/romanceabroad/ui/BaseUI.java
@@ -3,6 +3,7 @@ package com.romanceabroad.ui;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.opera.OperaDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -18,6 +19,8 @@ import org.testng.asserts.SoftAssert;
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BaseUI {
     protected WebDriver driver;
@@ -42,8 +45,10 @@ public class BaseUI {
 
     @BeforeMethod(groups = {"user", "admin", "chrome"}, alwaysRun = true)
     @Parameters({"browser", "version", "platform", "testbox"})
-    public void setup(@Optional("chrome") String browser, @Optional ("null") String version, @Optional ("null") String platform,
-                      @Optional ("null") String box, Method method) {
+    public void setup(@Optional("chrome") String browser,
+                      @Optional("null") String version,
+                      @Optional("null") String platform,
+                      @Optional("null") String box, Method method) {
         Reports.start(method.getDeclaringClass().getName() + " : " + method.getName());
 
 //        if(box.equalsIgnoreCase("local")) {
@@ -56,41 +61,44 @@ public class BaseUI {
             testBox = TestBox.SAUCE;
         }
 
-        switch(testBox) {
+        switch (testBox) {
             case LOCAL:
-            // Check if parameter passed from TestNG is 'firefox'
-            if (browser.equalsIgnoreCase("firefox")) {
-                // Set path to geckodriveradd
-                System.setProperty("webdriver.gecko.driver", "geckodriver");
-                // Create firefox instance
-                driver = new FirefoxDriver();
-            }
-//        // Check if parameter passed as 'IE'
-//        else if (browser.equalsIgnoreCase("ei")) {
-//            // Set path to EIdriver
-//            System.setProperty("webdriver.ie.driver", "IEDriverServer");
-//            driver = new InternetExplorerDriver();
-//            driver.manage().deleteAllCookies();
-//        }
-            // Check if parameter passed as 'chrome'
-            else if (browser.equalsIgnoreCase("chrome")) {
-                // Set path to geckodriver
-                System.setProperty("webdriver.chrome.driver", "chromedriver");
-                driver = new ChromeDriver();
-                driver.get("chrome://settings/clearBrowserData");
-            }
-            // Check if parameter passed as 'opera'
-            else if (browser.equalsIgnoreCase("opera")) {
-                // Set path to operadriverOld
-                System.setProperty("webdriver.opera.driver", "operadriver");
-                driver = new OperaDriver();
-                driver.manage().deleteAllCookies();
-            } else {
-                System.setProperty("webdriver.chrome.driver", "chromedriver");
-                driver = new ChromeDriver();
-                driver.get("chrome://settings/clearBrowserData");
-            }
-            break;
+                // Check if parameter passed from TestNG is 'firefox'
+                if (browser.equalsIgnoreCase("firefox")) {
+                    // Set path to geckoDriver
+                    System.setProperty("webdriver.gecko.driver", "geckodriver");
+                    // Create firefox instance
+                    driver = new FirefoxDriver();
+                }
+                // Check if parameter passed as 'chrome'
+                else if (browser.equalsIgnoreCase("chrome")) {
+                    // Set path to geckodriver
+                    System.setProperty("webdriver.chrome.driver", "chromedriver");
+                    driver.get("chrome://settings/clearBrowserData");
+                }
+                // Check if parameter passed as 'opera'
+                else if (browser.equalsIgnoreCase("opera")) {
+                    // Set path to operaDriverOld
+                    System.setProperty("webdriver.opera.driver", "operadriver");
+                    driver = new OperaDriver();
+                    driver.manage().deleteAllCookies();
+                } else if (browser.equalsIgnoreCase("mobile")) {
+                    // Set mobile optimization
+                    Map<String, String> mobileEmulation = new HashMap<String, String>();
+                    System.out.println("Mobile Chrome");
+                    mobileEmulation.put("deviceName", "Nexus 4");
+                    ChromeOptions chromeOptions = new ChromeOptions();
+                    chromeOptions.setExperimentalOption("mobileEmulation", mobileEmulation);
+                    System.setProperty("webdriver.chrome.driver", "chromedriver");
+                    driver = new ChromeDriver(chromeOptions);
+                    driver.get("chrome://settings/clearBrowserData");
+                    System.out.println(mobileEmulation.get("deviceName"));
+                } else {
+                    System.setProperty("webdriver.chrome.driver", "chromedriver");
+                    driver = new ChromeDriver();
+                    driver.get("chrome://settings/clearBrowserData");
+                }
+                break;
             case SAUCE:
                 DesiredCapabilities capabilities = new DesiredCapabilities();
                 capabilities.setCapability("username", "AnjaT");
@@ -127,12 +135,13 @@ public class BaseUI {
 
     @AfterMethod
     public void tearDown(ITestResult testResult) {
+        // Local Reports
         if (testResult.getStatus() == ITestResult.FAILURE) {
             Reports.fail(driver, testResult.getName());
         }
         Reports.stop();
 
-        //SauceLabs reports
+        // SauceLabs reports
         ((JavascriptExecutor) driver).executeScript
                 ("sauce:job-result=" + (testResult.isSuccess() ? "passed" : "failed"));
         driver.quit();
